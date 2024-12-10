d029827b1b63c2cc8b7e2223a18a872084577606
aspendav1s <aspenjdavis@gmail.com>
2024-12-11 09:26:56 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,9 +47,7 @@ if ingredients_list:
 
 #New section to import
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/orange")
-st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
 if ingredients_list:
     ingredients_string = ''
